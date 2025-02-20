a12c79fc5729203679c5db37f40b9c4bd0ea314f
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-02-20 21:09:44 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -12,4 +12,4 @@ echo "moodle is ready"
 # Give it another 3 seconds.
 sleep 3;
 
-curl --silent --fail http://app:8080 | grep '>Dockerized_Moodle<'
+curl --silent --fail http://app:8080/composer.json
