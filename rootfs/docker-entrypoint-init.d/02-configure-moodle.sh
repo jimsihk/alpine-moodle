51dee6e0233623433b77c00f0ffb1dec8f9fa28e
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-06-07 18:33:10 +0800

    Add more git debug logs

--- rootfs/docker-entrypoint-init.d/02-configure-moodle.sh
+++ rootfs/docker-entrypoint-init.d/02-configure-moodle.sh
@@ -283,11 +283,15 @@ if [ -z "$AUTO_UPDATE_MOODLE" ] || [ "$AUTO_UPDATE_MOODLE" = true ]; then
     if [ -z "$UPDATE_MOODLE_CODE" ] || [ "$UPDATE_MOODLE_CODE" = true ]; then
       echo "Checking moodle code version..."
       GIT_TRACE=1
-      git status
+      git -C "${WEB_PATH}" rev-parse HEAD
+      git -C "${WEB_PATH}" status
+      echo "Fetching moodle code..."
       git -C "${WEB_PATH}" fetch origin "$MODOLE_GIT_BRANCH" --depth=1 "$MOODLE_GIT_COMMIT"
-      git status
+      git -C "${WEB_PATH}" status
+      echo "Updating moodle code..."
       git -C "${WEB_PATH}" checkout "$MOODLE_GIT_COMMIT" -B "$MODOLE_GIT_BRANCH"
-      git status
+      git -C "${WEB_PATH}" rev-parse HEAD
+      git -C "${WEB_PATH}" status
       GIT_TRACE=0
       #/usr/libexec/moodle/clean-moodle-code "${WEB_PATH}"
     fi
